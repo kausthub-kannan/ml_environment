3a7c127a89ff94bd12869c1b496af8d0a088e389
Kausthub Kannan <99611070+kausthub-kannan@users.noreply.github.com>
2023-03-25 19:40:32 +0530

    Image Processing Functions added

--- helpers/helper_functions.py
+++ helpers/helper_functions.py
@@ -97,3 +97,53 @@ def supervised_metrics(y_pred, y_true=None, x=None):
     print(f"Completeness Score: {metrics.completeness_score(y_true, y_pred)}")
     print(f"Fowlkes Mallows Score: {metrics.fowlkes_mallows_score(y_true, y_pred)}")
     print(f"Homogeneity Score: {metrics.homogeneity_score(y_true, y_pred)}")
+
+import os
+import random
+import matplotlib.image as mpimg    
+
+#Image Pre-Processing
+def pre_process_image(path, image_shape, channels=3, norm_factor=255.):
+    '''Pre-Processing the Image before sending it to the model'''
+    img = tf.io.read_file(path)
+    img = tf.image.decode_image(img, channels=channels)
+    img = tf.image.resize(img, size = (image_shape, image_shape))
+    img = tf.expand_dims(img, axis=0)
+    img = img/norm_factor
+    return img
+
+def view_random_image(root_path,folder,class_folder):
+    path=root_path+'/'+folder+'/'+class_folder
+    rand=random.choice(os.listdir(path))
+    random_image=mpimg.imread(path+'/'+rand)
+    plt.imshow(random_image)
+    plt.title("File Name:" + rand)
+
+def custom_rounder(x,threshold):
+  if(x<threshold):
+    return 0
+  else:
+    return 1
+
+def random_tester(root_path, classes, model, class_type="binary"):
+    '''Random Class Folder Selection'''
+    path=root_path
+    class_folder=random.choice(os.listdir(path))
+
+    '''Random File Selection'''
+    folder_path='/content/drive/MyDrive/IntelliDoc/chest_xray/test/'+class_folder+'/'
+    rand=random.choice(os.listdir(folder_path))
+    file_path=folder_path+'/'+rand
+    random_image=mpimg.imread(file_path)
+
+    '''Prediction'''
+    predicted_value=model.predict(pre_process_image(file_path)) 
+    if(class_type=="binary"):
+      predicted_label=classes[custom_rounder(predicted_value)]
+    else:
+      predicted_label=classes[tf.math.round(predicted_value)]
+
+    '''Visualize'''
+    plt.imshow(random_image)
+    plt.title("Prediction:" + predicted_label +"\n" +"True class: "+ class_folder)
+    plt.show()

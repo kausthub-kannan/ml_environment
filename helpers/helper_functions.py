22f123d03718d65250a57ec52f225a30f89f6326
Kausthub Kannan <99611070+kausthub-kannan@users.noreply.github.com>
2023-04-03 23:08:38 +0530

    Bugs Fixed
    Image processing and Supervised Metrics Bugs fixed

--- helpers/helper_functions.py
+++ helpers/helper_functions.py
@@ -41,25 +41,11 @@ def confusion_matrix_plot(y_true, y_pred, size=16):
 
 
 def supervised_metrics(y_true, y_pred):
-    """Meterics for a Supervised Learning model:
-    TP -> True postivies
-    TN -> True negatives
-    FP -> False postivies
-    FN -> False negatives"""
-    TP = tf.count_nonzero(y_pred * y_true)
-    TN = tf.count_nonzero((y_pred - 1) * (y_true - 1))
-    FP = tf.count_nonzero(y_pred * (y_true - 1))
-    FN = tf.count_nonzero((y_pred - 1) * y_true)
-
-    precision = TP / (TP + FP)
-    recall = TP / (TP + FN)
-    f1 = 2 * precision * recall / (precision + recall)
-    accuracy = (TP + TN) / (TP + TN + FP + FN)
-
-    print(f"Accuracy: {accuracy}")
-    print(f"Precision: {precision}")
-    print(f"Recall: {recall}")
-    print(f"F1 Score: {f1}")
+    """Meterics for a Supervised Learning model:"""
+    print("Accuracy : {} %".format(accuracy_score(y_true, y_pred)*100))
+    print("F1 Score : {}".format(f1_score(y_true, y_pred, average='weighted')))
+    print("Recall : {}".format(recall_score(y_true, y_pred, average='weighted')))
+    print("Precision : {}".format(precision_score(y_true, y_pred, average='weighted')))
 
 #Model Helper Functions
 import datetime
@@ -79,13 +65,8 @@ def create_tensorboard_callback(dir_name, experiment_name):
   print(f"Saving TensorBoard log files to: {log_dir}")
   return tensorboard_callback
 
-#Unsupervised Clustering Metrics
 def supervised_metrics(y_pred, y_true=None, x=None):
-  """Meterics for a Unsupervised Learning model
-  TP -> True postivies
-  TN -> True negatives
-  FP -> False postivies
-  FN -> False negatives"""
+  """Meterics for a Unsupervised Learning model"""
   if(x):
     print("...........Score with respect to X............")
     print(f"Silohouette Score: {metrics.silhouette_score(x,y_pred)}")
@@ -103,7 +84,14 @@ import random
 import matplotlib.image as mpimg    
 
 #Image Pre-Processing
-def pre_process_image(path, image_shape, channels=3, norm_factor=255.):
+def view_random_image(root_path,folder,class_folder):
+    path=root_path+'/'+folder+'/'+class_folder
+    rand=random.choice(os.listdir(path))
+    random_image=mpimg.imread(path+'/'+rand)
+    plt.imshow(random_image)
+    plt.title("File Name: " + rand)
+    
+def pre_process_image(path, image_shape=224, channels=3, norm_factor=255.):
     '''Pre-Processing the Image before sending it to the model'''
     img = tf.io.read_file(path)
     img = tf.image.decode_image(img, channels=channels)
@@ -112,38 +100,62 @@ def pre_process_image(path, image_shape, channels=3, norm_factor=255.):
     img = img/norm_factor
     return img
 
-def view_random_image(root_path,folder,class_folder):
-    path=root_path+'/'+folder+'/'+class_folder
-    rand=random.choice(os.listdir(path))
-    random_image=mpimg.imread(path+'/'+rand)
-    plt.imshow(random_image)
-    plt.title("File Name:" + rand)
-
-def custom_rounder(x,threshold):
-  if(x<threshold):
-    return 0
-  else:
-    return 1
-
 def random_tester(root_path, classes, model, class_type="binary"):
     '''Random Class Folder Selection'''
     path=root_path
     class_folder=random.choice(os.listdir(path))
-
+    
     '''Random File Selection'''
-    folder_path='/content/drive/MyDrive/IntelliDoc/chest_xray/test/'+class_folder+'/'
+    folder_path=path+'/'+class_folder+'/'
     rand=random.choice(os.listdir(folder_path))
     file_path=folder_path+'/'+rand
     random_image=mpimg.imread(file_path)
-
+    
     '''Prediction'''
     predicted_value=model.predict(pre_process_image(file_path)) 
     if(class_type=="binary"):
-      predicted_label=classes[custom_rounder(predicted_value)]
+        predicted_label=classes[custom_rounder(predicted_value)]
     else:
-      predicted_label=classes[tf.math.round(predicted_value)]
-
+        index=tf.math.round(predicted_value).numpy()
+        index=np.argmax(index)
+        predicted_label=classes[index]
+        
     '''Visualize'''
     plt.imshow(random_image)
     plt.title("Prediction:" + predicted_label +"\n" +"True class: "+ class_folder)
     plt.show()
+    
+def loss_curve_plot(df):
+    """ Dataframe (df) is history of the fit of the NN model
+    The df consists of train and validation fit data
+    """
+    history = df.history
+    val_accuracy = history["val_accuracy"]
+    val_loss = history["val_loss"]
+    train_accuracy = history["accuracy"]
+    train_loss = history["loss"]
+    
+    """Accuracy Plot"""
+    plt.plot(train_accuracy, label="Train Accuracy")
+    plt.plot(val_accuracy, label="Validation Accuracy")
+    plt.title("Accuracy Curves")
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.legend()
+    plt.show()
+    
+    """Loss Plot"""
+    plt.plot(train_loss, label="Train loss")
+    plt.plot(val_loss, label="Validation loss")
+    plt.title("Loss Curves")
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend()
+    plt.show()
+    
+def confusion_matrix_plot(y_true, y_pred, figsize=(30,30)):
+    """"Confusion Matrix for true values and predicted values"""
+    cm = metrics.confusion_matrix(y_true, y_pred)
+    cm = cm.astype('float') / cm.sum(axis=1)
+    plt.figure(figsize = figsize)
+    sns.heatmap(cm, annot=True, cmap="crest")
